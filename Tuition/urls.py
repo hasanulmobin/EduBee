99aaf6210bd81371a7df67dda68f39d6018afe21
RitaAkhter86842 <ritaakter74821@gmail.com>
2026-04-17 23:52:18 +0600

    Create a review feature.

--- Tuition/urls.py
+++ Tuition/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
 path('dashboard/', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
 path('student/', TemplateView.as_view(template_name='student.html'), name='student'),
+path('review/', TemplateView.as_view(template_name='review.html'), name='review'),
 ]
