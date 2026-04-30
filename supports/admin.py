d7849d07273805cecfe63baab583c3c92f3803f7
RitaAkhter86842 <ritaakter74821@gmail.com>
2026-04-30 21:57:16 +0600

    create supports admin site

--- supports/admin.py
+++ supports/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review, Message, Report, Notification
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display  = ['student', 'teacher', 'rating', 'date']
+    list_filter   = ['rating']
+    search_fields = ['student_userusername', 'teacheruser_username']
