4324ed5be5690bb1d19ecd4625b718eac4f8e01f
Shakil Ahmed <shakilahmed11468@gmail.com>
2026-05-03 00:28:02 +0600

    updates

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
+from .models import TuitionPost, TuitionApplication, Payment
 
-# Register your models here.
+
+@admin.register(TuitionPost)
+class TuitionPostAdmin(admin.ModelAdmin):
+    list_display = [
+        'subject',
+        's_class',
+        'version',
+        'mode',
+        'salary',
+        'status',
+        'date'
+    ]
+
+    list_filter = [
+        'status',
+        'mode',
+        'gender'
+    ]
+
+    search_fields = [
+        'subject',
+        's_class',
+        'student__user__username'
+    ]
+
+
+@admin.register(TuitionApplication)
+class TuitionApplicationAdmin(admin.ModelAdmin):
+    list_display = [
+        'teacher',
+        'tuition_post',
+        'status',
+        'date'
+    ]
+
+    list_filter = ['status']
+
+    search_fields = [
+        'teacher__user__username',
+        'tuition_post__subject'
+    ]
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = [
+        'student',
+        'teacher',
+        'tuition_post',
+        'amount',
+        'method',
+        'status',
+        'paid_date'
+    ]
+
+    list_filter = [
+        'status',
+        'method'
+    ]
+
+    search_fields = [
+        'student__user__username',
+        'teacher__user__username',
+        'tuition_post__subject'
+    ]
